2d04f0a41e573a018e1a2af74641ace469576cdb
sundogx <sundogx@gmail.com>
2022-02-19 13:51:39 -0500

    Update greedy_bot.py

--- bots/greedy_bot.py
+++ bots/greedy_bot.py
@@ -1,6 +1,7 @@
 import sys
 
 import random
+import math
 
 from src.player import *
 from src.structure import *
@@ -18,15 +19,23 @@ class MyPlayer(Player):
     def play_turn(self, turn_num, map, player_info):
         self.width = len(map)
         self.height = len(map[0])
-        self.find_tiles
-
-
-        for i in range(self.width, self)
-
+        self.find_tiles(map, player_info)
+        path = self.compute_cluster(map, player_info)
+        remaining_money = player_info.money
+        for i, (x, y) in enumerate(path)
+            if i < len(path) - 1:
+                if remaining_money >= map[x][y].passability * StructureType.ROAD.get_base_cost():
+                    self.build(StructureType.ROAD, x, y)
+                else:
+                    continue
+            else:
+                if remaining_money >= map[x][y].passability * StructureType.TOWER.get_base_cost():
+                    self.build(StructureType.TOWER, x, y)
         return
 
     def find_tiles(self, map, player_info):
         self.my_generators = []
+        self.my_structs = []
         self.other_structs = []
         for x in range(self.MAP_WIDTH):
             for y in range(self.MAP_HEIGHT):
@@ -42,16 +51,33 @@ class MyPlayer(Player):
                         self.other_structs.append((x, y))
 
     def compute_cluster(self, map, player_info):
-        for x in range(self.width - self.cluster_size):
-            for y in range(self.height - self.cluster_size):
-                cluster_population = 0
-                for i in range(self.cluster_size):
-                    for j in range(self.cluster_size):
-                        cluster_population += map[x + i][y + j].population
+        max_ratio = 0
+        max_path = []
+        for x in range(self.width):
+            for y in range(self.height):
+                cluster_population = self.cluster_population(self, map, x, y, [[-2, 0], [-1, -1], [-1, 0], [-1, 1], [0, -2], [0, -1], [0, 0], [0, 1], [0, 2], [1, -1], [1, 0], [1, 1], [2, 0]])
+                if cluster_population > 0:
+                    distance, path = self.cluster_path(self, map, x, y)
+                    if distance > 0 and cluster_population / distance > max_ratio:
+                        max_ratio = cluster_population / distance
+                        max_path = path
+        return max_path
 
-    def cluster_distance(self, map, x_2, y_2):
-        for 
+    def cluster_population(self, map, x, y, ds):
+        cluster_population = 0
+        for d in ds:
+            cluster_population += map[x + d[0]][y + d[1]].population
+        return cluster_population
+
+    def cluster_path(self, map, x, y):
+        min_distance = math.inf
+        min_path = []
+        for s in self.my_structs:
+            distance, path = path(map, s[0], s[1], x, y)
+            if distance <= min_distance:
+                min_path = path
+        return min_distance, min_path
     
-    def distance(self, map, x_1, y_1, x_2, y_2):
+    def path(self, map, x_1, y_1, x_2, y_2):
         # todo
         return 0
